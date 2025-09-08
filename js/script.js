2721e42935a45c28a9c7edfb455f99958b9a91de
Casen Ward <casenward@gmail.com>
2025-09-08 13:11:31 -0400

    photo dropdown finished

--- js/script.js
+++ js/script.js
@@ -15,85 +15,6 @@ const home_carousel = [
     //'images/home_carousel/Photo_10.jpg'
 ];
 
-/**
-const sports_carousel = [
-    'images/sports_carousel/S_Photo_1.jpg',
-    'images/sports_carousel/S_Photo_2.jpg',
-    'images/sports_carousel/S_Photo_3.jpg',
-    'images/sports_carousel/S_Photo_4.jpg',
-    'images/sports_carousel/S_Photo_5.jpg',
-    'images/sports_carousel/S_Photo_6.jpg',
-    'images/sports_carousel/S_Photo_7.jpg',
-    'images/sports_carousel/S_Photo_8.jpg',
-    'images/sports_carousel/S_Photo_9.jpg',
-    'images/sports_carousel/S_Photo_10.jpg'
-];
-
-const seniorPortraits_carousel = [
-    'images/senior_portraits/SP_Photo_1.jpg',
-    'images/senior_portraits/SP_Photo_2.jpg',
-    'images/senior_portraits/SP_Photo_3.jpg',
-    'images/senior_portraits/SP_Photo_4.jpg',
-    'images/senior_portraits/SP_Photo_5.jpg',
-    'images/senior_portraits/SP_Photo_6.jpg',
-    'images/senior_portraits/SP_Photo_7.jpg',
-    'images/senior_portraits/SP_Photo_8.jpg',
-    'images/senior_portraits/SP_Photo_9.jpg',
-    'images/senior_portraits/SP_Photo_10.jpg'
-];
-
-const couples_carousel = [
-    'images/couples/C_Photo_1.jpg',
-    'images/couples/C_Photo_2.jpg',
-    'images/couples/C_Photo_3.jpg',
-    'images/couples/C_Photo_4.jpg',
-    'images/couples/C_Photo_5.jpg',
-    'images/couples/C_Photo_6.jpg',
-    'images/couples/C_Photo_7.jpg',
-    'images/couples/C_Photo_8.jpg',
-    'images/couples/C_Photo_9.jpg',
-    'images/couples/C_Photo_10.jpg'
-];
-
-const events_carousel = [
-    'images/family/E_Photo_1.jpg',
-    'images/family/E_Photo_2.jpg',
-    'images/family/E_Photo_3.jpg',
-    'images/family/E_Photo_4.jpg',
-    'images/family/E_Photo_5.jpg',
-    'images/family/E_Photo_6.jpg',
-    'images/family/E_Photo_7.jpg',
-    'images/family/E_Photo_8.jpg',
-    'images/family/E_Photo_9.jpg',
-    'images/family/E_Photo_10.jpg'
-];
-
-*/
-
-let currentCarousel = home_carousel;
-
-function selectCategory(category) {
-    switch (category) {
-        case 'Sports':
-            currentCarousel = sports_carousel;
-            break;
-        case 'Senior Pictures':
-            currentCarousel = seniorPortraits_carousel;
-            break;
-        case 'Couples':
-            currentCarousel = couples_carousel;
-            break;
-        case 'Events':
-            currentCarousel = events_carousel;
-            break;
-        default:
-            currentCarousel = home_carousel;
-    }
-    currentPhotoIndex = 0;
-    updatePhoto();
-    startAutoRotate();
-}
-
 function startAutoRotate() {
     // Clear any existing interval
     if (autoRotateInterval) {
@@ -104,13 +25,13 @@ function startAutoRotate() {
 }
 
 function previousPhoto() {
-    currentPhotoIndex = (currentPhotoIndex - 1 + currentCarousel.length) % currentCarousel.length;
+    currentPhotoIndex = (currentPhotoIndex - 1 + home_carousel.length) % home_carousel.length;
     updatePhoto();
     startAutoRotate(); // Restart the interval
 }
 
 function nextPhoto() {
-    currentPhotoIndex = (currentPhotoIndex + 1) % currentCarousel.length;
+    currentPhotoIndex = (currentPhotoIndex + 1) % home_carousel.length;
     updatePhoto();
     startAutoRotate(); // Restart the interval
 }
@@ -121,15 +42,15 @@ function updatePhoto() {
     const nextPhoto = document.getElementById('next-photo');
     
     // Update current photo
-    currentPhoto.src = currentCarousel[currentPhotoIndex];
+    currentPhoto.src = home_carousel[currentPhotoIndex];
 
     // Update previous photo
-    const previousIndex = (currentPhotoIndex - 1 + currentCarousel.length) % currentCarousel.length;
-    previousPhoto.src = currentCarousel[previousIndex];
+    const previousIndex = (currentPhotoIndex - 1 + home_carousel.length) % home_carousel.length;
+    previousPhoto.src = home_carousel[previousIndex];
 
     // Update next photo
-    const nextIndex = (currentPhotoIndex + 1) % currentCarousel.length;
-    nextPhoto.src = currentCarousel[nextIndex];
+    const nextIndex = (currentPhotoIndex + 1) % home_carousel.length;
+    nextPhoto.src = home_carousel[nextIndex];
 }
 
 // Initialize carousel
